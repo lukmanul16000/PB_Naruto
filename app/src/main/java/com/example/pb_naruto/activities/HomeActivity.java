34113aa5db8a591232c28bb0620cd9273d5eb85b
lukmanul160 <lukmanul160@gmail.com>
2020-09-24 12:40:07 +0700

    commit add homepage <null image>

--- app/src/main/java/com/example/pb_naruto/activities/HomeActivity.java
+++ app/src/main/java/com/example/pb_naruto/activities/HomeActivity.java
@@ -1,16 +1,52 @@
 package com.example.pb_naruto.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.pb_naruto.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private CardView card1,card2,card3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        card1 =(CardView) findViewById(R.id.card1);
+        card2 =(CardView) findViewById(R.id.card2);
+        card3 =(CardView) findViewById(R.id.card3);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent explicit = new Intent(getApplicationContext(), ListBatik.class);
+//                startActivity(explicit);
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent explicit = new Intent(getApplicationContext(), Batik.class);
+//                startActivity(explicit);
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(),"3",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
