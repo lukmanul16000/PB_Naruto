c6304c0b2e632fc1d690536f1814ec5225e7d4a6
lukmanul160 <lukmanul160@gmail.com>
2020-09-24 09:13:04 +0700

    membuat OnbuildLanding(belum termasuk image dan kalimat)

--- app/src/main/java/com/example/pb_naruto/activities/HomeActivity.java
+++ app/src/main/java/com/example/pb_naruto/activities/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.pb_naruto;
+package com.example.pb_naruto.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.pb_naruto.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
