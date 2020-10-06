527ff20fc0181b158a9a5aea59963e93a1017bbf
Ahmad Ardin <09031381823073@student.unsri.ac.id>
2020-10-06 12:40:36 +0700

    Desain Dashboard Home

---
+++ app/src/main/java/com/example/pb_naruto/ArdinActivity.java
@@ -0,0 +1,14 @@
+package com.example.pb_naruto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArdinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ardin);
+    }
+}
