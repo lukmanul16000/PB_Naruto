e04beada1210ddadad0fc009dbdd44408715064c
ASUS <hagis2001@gmail.com>
2020-11-25 21:54:08 +0700

    app veri

--- app/src/main/java/com/example/pb_naruto/activities/SplashScreen.java
+++ app/src/main/java/com/example/pb_naruto/activities/SplashScreen.java
@@ -3,6 +3,9 @@ package com.example.pb_naruto.activities;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
+
+import com.example.pb_naruto.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
@@ -14,14 +17,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_splash_screen);
 
-        setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
                 //setelah loading maka akan langsung berpindah ke home activity
-                Intent home=new Intent(MainActivity.this, home.class);
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(home);
                 finish();
 
