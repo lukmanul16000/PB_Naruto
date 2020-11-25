5a7278efebd848a6f029cf401db2b3f96a64bfd2
ASUS <hagis2001@gmail.com>
2020-11-25 22:00:52 +0700

    app versi2

---
+++ app/src/main/java/com/example/pb_naruto/activities/version_app.java
@@ -0,0 +1,14 @@
+package com.example.pb_naruto.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class version_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_version_app);
+    }
+}
